c77963fc20f15696a6efa0a50571e86b8c6247c6
LostIsLife <c13kdx17@hotmail.com>
2017-04-27 15:51:17 +0100

    Added an untested bmp_loader to load bmp into memory. Added a texture.bmp to use Linked bmp_loader to cg_proj1 and changed makefile Next, Create Shader to load texture

---
+++ bmp_loader.c
@@ -0,0 +1,67 @@
+
+#include "bmp_loader.h"
+
+bmp new_bmp( char* n_filename )
+{
+	bmp result;
+	strcpy(result.filename,n_filename);
+	result.fl = NULL;
+	result.width = 0;
+	result.height = 0;
+	result.dataPos = 0;
+	result.image_size = 0;
+	for (int cicle = 0; cicle < BMP_H_SIZE; cicle++)
+		result.header[cicle] = '0';
+	result.data = NULL;
+	return result;
+}
+
+int error_check(bmp target, int verbose)
+{
+	if (target.fl == NULL)
+	{
+		if (verbose) printf (" ERROR. Could not find the file.\n");
+		return 1;
+	}
+
+	if (fread (target.header, 1, 54, target.fl) != 54)
+	{
+		if (verbose) printf(" ERROR. Header does not have 54 bytes. May not be a real bmp file. Stopping the execution.\n");
+		return 2;
+	}
+	
+	if (target.header[0] != 'B' || target.header[1] != 'M')
+	{
+		if (verbose) printf (" ERROR. File did not start with a BM.\n");
+		return 3;
+	}
+	return 0;
+}
+
+int parse_file(bmp target, int verbose)
+{
+	int error = -1;
+	target.fl = fopen( target.filename, "rb" );
+	
+	if ( (error = error_check(target, verbose) ) != 0)
+		return error;
+
+	target.dataPos = 		*(int*)&(target.header[0x0A]);
+	target.image_size = 	*(int*)&(target.header[0x22]);
+	target.width = 		*(int*)&(target.header[0x12]);
+	target.height = 		*(int*)&(target.header[0x16]);
+	
+	if (target.image_size == 0) target.image_size = target.width * target.height * 3;
+	if (target.dataPos == 0) 	 target.dataPos = 54;
+
+	if (target.image_size <= 0)
+		return 4;
+
+	target.data = malloc(sizeof(unsigned char) * target.image_size );
+
+	fread(target.data, 1, target.image_size, target.fl);
+	fclose(target.fl);
+	return 0;
+}
+
+
---
+++ bmp_loader.h
@@ -0,0 +1,23 @@
+#ifndef BMP_LOADER
+#define BMP_LOADER
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define BMP_H_SIZE 54
+
+typedef struct
+{
+	unsigned char filename[50], header[ BMP_H_SIZE ];
+  	unsigned int width, height, dataPos, image_size;
+	FILE* fl;
+	unsigned char* data;
+} bmp;
+
+bmp new_bmp( char* n_filename );
+int error_check(bmp target, int verbose);
+int parse_file(bmp target, int verbose);
+
+
+#endif
